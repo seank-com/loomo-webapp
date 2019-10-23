39283608a4faeb7e92cf304748e454aaa0d42e42
Sean Kelly <home@seank.com>
2019-10-23 12:19:34 -0700

    implement store and detect portion of API

--- web/src/routes/api.js
+++ web/src/routes/api.js
@@ -1,29 +1,115 @@
 var express = require('express');
+var utils = require('../utils/utils');
+var crypto = require('crypto');
+var https = require('https');
+var path = require('path');
+var fs = require('fs');
+var timers = require('timers');
+var url = require('url');
 
 var router = express.Router();
 
-router.get('/posters', function(req, res) {
-  var result = [], i, ones, tens, hundreds, thousands;
+function receiveBody(req, callback) {
+  var chunks = [];
 
-  for (i = 1; i < 10001; i += 1) {
-    ones = i % 10;
-    tens = Math.floor((i % 100) / 10);
-    hundreds = Math.floor((i % 1000) / 100);
-    thousands = Math.floor((i % 10000) / 1000);
+  req.on('error', callback);
 
-    result.push({
-      title: " Sample Poster " + i,
-      tags: [
-        ["tag-0", "tag-I", "tag-II", "tag-III", "tag-IV", "tag-V", "tag-VI", "tag-VII", "tag-VIII", "tag-IX"][ones],
-        ["tag-00", "tag-X", "tag-XX", "tag-XXX", "tag-XL", "tag-L", "tag-LX", "tag-LXX", "tag-LXXX", "tag-XC"][tens],
-        ["tag-000", "tag-C", "tag-CC", "tag-CCC", "tag-CD", "tag-D", "tag-DC", "tag-DCC", "tag-DCCC", "tag-CM"][hundreds],
-        ["tag-0000", "tag-M", "tag-MM", "tag-MMM", "tag-MV-", "tag-V-", "tag-V-M", "tag-V-MM","tag-V-MM","tag-MX-"][thousands]
-      ]
+  req.on('data', function(chunk) {
+    chunks.push(chunk);
+  });
+
+  req.on('end', function() {
+    var buffer = Buffer.concat(chunks);
+    callback(null, buffer);
+  });
+}
+
+function executeRequest(options, data, callback) {
+  var req = https.request(options, (res) => {
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      callback(null, res);
+    } else {
+      // All other errors
+      err = new Error("Bad statusCode " + res.statusCode);
+      err.res = res;
+      callback(err, null);
+    }
+  });
+  req.on('error', callback);
+
+  // Write data to request body
+  req.write(data);
+  req.end();  
+}
+
+function uploadBlob(name, data, datatype, callback) {
+  var req = {},
+  uri = process.env.BLOB_CONTAINER_URI_PATH + name +
+  process.env.BLOB_SAS_QUERY_STRING,
+  options = url.parse(uri);
+
+  options.method = "PUT";
+  options.headers = { 
+    "x-ms-blob-type": "BlockBlob",
+    "Content-Type": datatype,
+    "Content-Length": Buffer.byteLength(data)
+  };
+
+  executeRequest(options, data, callback);
+}
+
+function detectFace(imageURI, callback) {
+  var req = {},
+    uri = process.env.COGNITIVE_SERVICES_DETECT_FACE_URI,
+    options = url.parse(uri),
+    body = JSON.stringify({
+      url: imageURI
     });
+  
+    options.method = "POST";
+    options.headers = {
+      "Ocp-Apim-Subscription-Key": process.env.COGNITIVE_SERVICES_SUBSCRIPTION_KEY,
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(body)
+    };
+
+    executeRequest(options, body, callback);
   }
 
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(result, null, 2));
+router.put('/recognize', function(req, res) {
+  var uuid = utils.uuidgen(),
+    name = uuid + ".jpg";
+
+  receiveBody(req, function(err, buffer) {
+    if (err) {
+      res.status(500).json({ error: err.message});
+    } else {
+      uploadBlob(name, buffer, "image/jpeg", function(err, bres) {
+        if (err) {
+          res.status(500).json({
+            error: err.message
+          });
+        } else {
+          detectFace(process.env.BLOB_CONTAINER_URI_PATH + name, function(err, detectRes) {
+            if (err) {
+              res.status(500).json({error: err.message});
+            } else {
+              receiveBody(detectRes, function(err, buffer) {
+                var jsonName = uuid + ".json";
+                uploadBlob(jsonName, buffer.toString(), "application/json", function(err, bres) {
+                  if (err) {
+                    res.status(500),json({ error: err.message});
+                  } else {
+                    res.status(200).json({ id: uuid });
+                  }
+                });  
+              });
+            }
+          });
+        }
+      });    
+    }
+  });
 });
 
 module.exports = router;
