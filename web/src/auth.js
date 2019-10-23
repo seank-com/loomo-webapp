62bdaf9c586e022dfc31661d115a35a814a7a2f6
Sean Kelly <home@seank.com>
2019-10-23 12:00:18 -0700

    implement local auth strategy

--- web/src/auth.js
+++ web/src/auth.js
@@ -2,8 +2,39 @@ var cookieSession = require('cookie-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var flash = require('connect-flash');
+var crypto = require('crypto');
 
-var User = require('./models/user');
+var admin = {
+  id:1,   
+  username: 'admin',
+  hash: 'xi9q8bqXbKZb9encpIvHYn9gu9JnCDjM2Wj3tHG44B0=',
+  avatar: '11.1-robot.png',
+  fullname: 'Administrator',
+  jobtitle: 'Digital Slave',
+  createdAt: new Date(2019, 10, 22),
+  updatedAt: Date.now()
+};
+
+function validateUser(username, password, done) {
+  var hash = crypto.createHash('sha256').update(password).digest('base64');
+  if (admin.username === username && admin.hash === hash) {
+   return done(null, admin);
+  } else {
+   return done(null, false);
+  }
+}
+
+function serializeUser(user, done) {
+  done(null,admin.id);
+}
+
+function deserializeUser(id, done) {
+  if (admin.id === id) {
+    done(null, admin);
+  } else {
+    done(new Error("cannot deserialize user"))
+  }
+}
 
 function init(app) {
   // cookies are used to remember who is logged in
@@ -21,9 +52,14 @@ function init(app) {
   app.use(flash());
 
   // passport config
-  passport.use(new LocalStrategy(User.authenticate()));
-  passport.serializeUser(User.serializeUser());
-  passport.deserializeUser(User.deserializeUser());
+  passport.use(new LocalStrategy(validateUser));
+  passport.serializeUser(serializeUser);
+  passport.deserializeUser(deserializeUser);
 }
 
 module.exports.init = init;
+
+
+
+
+
